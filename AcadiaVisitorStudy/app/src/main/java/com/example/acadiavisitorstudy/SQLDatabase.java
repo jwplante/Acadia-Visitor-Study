896160e6dd12f166a24a16139093cda5434ac766
James Plante <jplante@wpi.edu>
2019-06-11 10:06:54 -0400

    Got JSON to server working

--- AcadiaVisitorStudy/app/src/main/java/com/example/acadiavisitorstudy/SQLDatabase.java
+++ AcadiaVisitorStudy/app/src/main/java/com/example/acadiavisitorstudy/SQLDatabase.java
@@ -3,22 +3,32 @@ package com.example.acadiavisitorstudy;
 import android.location.Location;
 import android.util.Log;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
-import android.app.ProgressDialog;
 import android.os.AsyncTask;
-import android.os.Bundle;
 
+import java.io.BufferedReader;
 import java.io.DataOutputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.net.URLEncoder;
 import java.util.ArrayList;
+import java.util.Random;
 
-public class SQLDatabase implements ILocationProcessor{
+public class SQLDatabase implements ILocationProcessor {
 
-    SQLDatabase(){}
-
+    private final String url = "http://acadiatrails.wpi.edu/"; // URL to send the data to.
+    private int uid;
     private static final String TAG = "SQLDatabase";
+
+    SQLDatabase() {
+        // Get a random uid (temporary fix)
+        Random r = new Random();
+        this.uid = r.nextInt(Integer.MAX_VALUE);
+    }
 
 
     /***
@@ -28,26 +38,32 @@ public class SQLDatabase implements ILocationProcessor{
      * @return
      */
     @Override
-    public boolean processDataPoints(ArrayList<Location> locations){
-       // Currently a stub right now to test the logic.
+    public boolean processDataPoints(ArrayList<Location> locations) {
+        // Currently a stub right now to test the logic.
         Log.i(TAG, "Processing bundle of size " + locations.size());
-        String url = "http://75.134.234.187/acadia.php";
-        for (Location l : locations) {
-            JSONObject blob = new JSONObject();
-            try {
-                blob.put("Latitude", l.getLatitude());
-                blob.put("Longitude", l.getLongitude());
-                blob.put("Speed", l.getSpeed());
-                blob.put("Time", l.getTime());
-                AsyncTaskRunner serverUpload = new AsyncTaskRunner();
-                serverUpload.execute(url, blob.toString());
-            } catch (JSONException e) {
-                e.printStackTrace();
+        JSONObject jobj = new JSONObject();
+        try {
+            jobj.put("user", uid);
+            jobj.put("operation","location");
+            JSONArray jarray = new JSONArray();
+            // Put all location objects in the JSON array
+            for (Location l : locations) {
+                JSONObject blob = new JSONObject();
+                blob.put("latitude", l.getLatitude());
+                blob.put("longitude", l.getLongitude());
+                blob.put("velocity", l.getSpeed());
+                blob.put("time", l.getTime());
+                jarray.put(blob);
+                Log.i(TAG, "Location point: " + LocationHelper.getLocationText(l) + "has been uploaded to the server.");
             }
-            Log.i(TAG, "Location point: " + LocationHelper.getLocationText(l) + "has been uploaded to the server.");
-
+            jobj.put("data",jarray);
+            AsyncTaskRunner serverUpload = new AsyncTaskRunner();
+            serverUpload.execute(url, jobj.toString());
+            return true;
+        } catch (JSONException e) {
+            e.printStackTrace();
+            return false;
         }
-        return true;
     }
 
     /***
@@ -56,7 +72,7 @@ public class SQLDatabase implements ILocationProcessor{
      * @return
      */
     @Override
-    public boolean processSurvey(ArrayList<ISurveyQuestion> questions) {
+    public boolean processSurvey (ArrayList < ISurveyQuestion > questions) {
         // Currently a stub right now to test the logic.
         int i = 0;
         Log.i(TAG, "processSurvey: Processing " + questions.size() + " Questions");
@@ -78,32 +94,24 @@ public class SQLDatabase implements ILocationProcessor{
             HttpURLConnection httpURLConnection = null;
             try {
 
-                URL url =  new URL(params[0]);
+                URL url = new URL(params[0]);
                 httpURLConnection = (HttpURLConnection) url.openConnection();
                 httpURLConnection.setRequestMethod("POST");
-
+                httpURLConnection.setRequestProperty("Content-Type", "application/json; charset=UTF-8");
+                httpURLConnection.setRequestProperty("Accept", "application/json");
                 httpURLConnection.setDoOutput(true);
+                httpURLConnection.connect();
 
-                DataOutputStream wr = new DataOutputStream(httpURLConnection.getOutputStream());
-                wr.writeBytes("PostData=" + params[1]);
-                wr.flush();
-                wr.close();
-
-//                InputStream in = httpURLConnection.getInputStream();
-//                InputStreamReader inputStreamReader = new InputStreamReader(in);
-//
-//                int inputStreamData = inputStreamReader.read();
-//                while (inputStreamData != -1) {
-//                    char current = (char) inputStreamData;
-//                    inputStreamData = inputStreamReader.read();
-//                    resp += current;
-//                }
+                DataOutputStream os = new DataOutputStream(httpURLConnection.getOutputStream());
+                os.writeBytes(params[1]);
+                os.flush();
+                os.close();
 
             } catch (Exception e) {
                 e.printStackTrace();
                 resp = e.getMessage();
             } finally {
-                if (httpURLConnection != null){
+                if (httpURLConnection != null) {
                     httpURLConnection.disconnect();
                 }
             }
